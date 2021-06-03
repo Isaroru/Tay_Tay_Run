442563e20d128da6f94d2da78f63af2974f605f4
josemoncada87 <josemoncada87@hotmail.com>
2021-06-03 10:54:20 -0500

    Fix Taylor move function

--- Tay.js
+++ Tay.js
@@ -5,13 +5,18 @@ class Tay{
         this.xPos = xPos; 
         this.yPos = yPos;
         this.mapReference = mapReference;
+        // posición inicial // remover despues para asignarla por nivel
+        this.pjCol = 0; // los valores se están quedando fijos, por eso no se mueve
+        this.pjFil = 0; // los valores se están quedando fijos, por eso no se mueve
+        this.xPos = (this.pjCol * 100) + 0; // los valores se están quedando fijos, por eso no se mueve
+        this.yPos = (this.pjFil * 100) + 100; // los valores se están quedando fijos, por eso no se mueve
     }
 
     mostrar(){
-        this.pjCol = 0;
-        this.pjFil = 0;
-        this.xPos = (this.pjCol * 100) + 0; 
-        this.yPos = (this.pjFil * 100) + 100; 
+    //    this.pjCol = 0; // los valores se están quedando fijos, por eso no se mueve
+    //    this.pjFil = 0; // los valores se están quedando fijos, por eso no se mueve
+    //    this.xPos = (this.pjCol * 100) + 0; // los valores se están quedando fijos, por eso no se mueve
+    //    this.yPos = (this.pjFil * 100) + 100; // los valores se están quedando fijos, por eso no se mueve
         image(tay1, this.xPos, this.yPos);
         image(llave,610,210,80,80);
         image(cd,10,610,80,80);
@@ -19,8 +24,8 @@ class Tay{
     }
 
     mover(){
-        if(keyIsPressed){
-        switch (keyCode) {
+        //if(keyIsPressed){ // esto ya se está validando al llamar al método mover                
+        switch (keyCode) {            
             case LEFT_ARROW: // izquierda
             if (this.pjCol - 1 >= 0) {
                 if (this.mapReference[this.pjFil][this.pjCol - 1] === 0) {
@@ -52,7 +57,7 @@ class Tay{
             }
             this.xPos = (this.pjCol * 100) + 0;
             this.yPos = (this.pjFil * 100) + 100;
-        }
+        //}
     }
 
     getXPos(){
